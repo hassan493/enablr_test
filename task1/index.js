fb89a926ffed465c4165fef68ef4b82c2078cefb
Hassan Yousaf <>
2023-09-22 02:30:06 +0500

    completed task 1

---
+++ task1/index.js
@@ -0,0 +1,23 @@
+function getAlternateMondays(fromDate, toDate) {
+  //initialize resultant array
+  let alternateMondays = [];
+  
+  //initialize date objects
+  let startDate = new Date(fromDate);
+  let endDate = new Date(toDate);
+  
+  // Iterate throught start date by adding 1 each day to find first Monday
+  while (startDate.getDay() !== 1) {
+    startDate.setDate(startDate.getDate() + 1);
+  }
+  
+  // Loop through alternate Mondays until last date 
+  while (startDate <= endDate) {
+    alternateMondays.push(new Date(startDate));
+    startDate.setDate(startDate.getDate() + 14); // add 14 to skip to alternate Monday
+  }
+  
+  return alternateMondays;
+}
+
+console.log(getAlternateMondays('2023-08-01', '2023-08-21'))
